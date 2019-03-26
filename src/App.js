bcdb2b891081c786d7c0c0db1b29d2709f141685
doodoo <doodoo@drs-MacBook-Pro.local>
2019-03-26 09:14:01 -0600

    cleaned up and about to implement animations

--- src/App.js
+++ src/App.js
@@ -6,51 +6,52 @@ import Colors from './data/colors.json';
 import Tile from './components/Tile.js';
 
 class App extends Component {
-constructor(props){
-  super(props)
-  this.state= {
-    colors: [],
-    animals: [],
-    rows: 5,
-    columns: 5
-  }
-}
-componentWillMount(){
-  this.setState({animals: Animals, colors: Colors});
-}
-
-
-renderGrid() {
-  const { colors, animals, rows, columns } = this.state;
-  const grid = {
-      width: '100%',
-      height: '100%',
-      display: 'grid',
-      gridTemplateRows: `repeat(${rows}, calc(100vh / ${rows}))`
-   }
-   const row = {
-       display: 'grid',
-       gridTemplateColumns: `repeat(${columns}, calc(100vw / ${columns}))`
-   }
-    
-  let i = 0;
-  let tiles = [];
-  while ( i < rows) {
-    let j = 0;
-    let row = [];
-    while (j < columns) {
-      let id = i+'-'+j;
-      row.push(<Tile id={id} key={id} animals={animals} colors={colors}/>);
-      j++;
+  constructor(props){
+    super(props)
+    this.state= {
+      colors: [],
+      animals: [],
+      rows: 5,
+      columns: 5
     }
-    tiles.push(row);
-    i++;
   }
-  
-  return ( <div className ='grid' style={grid}>{tiles.map((rowArr, index) => {
-    return <div key={index} className='row' style={row}>{rowArr}</div>;
-  })}</div> )
-}
+  componentWillMount(){
+    this.setState({animals: Animals, colors: Colors});
+  }
+
+  renderGrid() {
+    const { colors, animals, rows, columns } = this.state;
+    const grid = {
+        width: '100%',
+        height: '100%',
+        display: 'grid',
+        gridTemplateRows: `repeat(${rows}, calc(100vh / ${rows}))`
+    }
+    const row = {
+        display: 'grid',
+        gridTemplateColumns: `repeat(${columns}, calc(100vw / ${columns}))`,
+    }
+      
+    let i = 0;
+    let tiles = [];
+    while ( i < rows) {
+      let j = 0;
+      let row = [];
+      while (j < columns) {
+        let id = i+'-'+j;
+        row.push(<Tile id={id} key={id} animals={animals} colors={colors}/>);
+        j++;
+      }
+      tiles.push(row);
+      i++;
+    }
+    
+    return ( <div className ='grid' style={grid}>{tiles.map((rowArr, index) => {
+      return <div key={index} className='row' style={row}>{rowArr}</div>;
+    })}</div> )
+  }
+
+
   render() {
     return (
       <main>
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-// import '../styles/tile.css';
 
 class Tile extends Component {
     constructor(props) {
@@ -11,28 +10,30 @@ class Tile extends Component {
             nextColor: ''
         }
     }
-    componentDidMount() {
+    componentWillMount() {
         const animal = this.props.animals[Math.floor(Math.random()*this.props.animals.length)];
         const color = this.props.colors[Math.floor(Math.random()*this.props.colors.length)];
         const nextAnimal = this.props.animals[Math.floor(Math.random()*this.props.animals.length)];
-        const nextColor = this.props.colors[Math.floor(Math.random()*this.props.colors.length)];
-        console.warn("this.props = ",this.props);
-        
+        const nextColor = this.props.colors[Math.floor(Math.random()*this.props.colors.length)];        
 
         this.setState({animal, color, nextAnimal, nextColor})
-        // console.warn('animal = ', animal);
-        // console.warn('color = ', color);
     }
-
-    render(){
+    animateTiles(){
         const { animal, color } = this.state;
+        console.warn("this.state = ", this.state);
+        
+        return ( <div className="card" style={{background: color}}>
+                    <p className="text">
+                        {animal}
+                    </p> 
+                </div> );
+    }
+    render(){
         
         return (
-                <div className="card"style={{background: color}}>
-                <p className="text">
-                    {animal}
-                    </p> 
-                </div>
+            <div className="tile">
+                {this.animateTiles()}
+            </div>
         );
     }
 }
