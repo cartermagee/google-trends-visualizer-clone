6a701103315f95d082166ee3f9691251b573ab0b
doodoo <doodoo@drs-MacBook-Pro.local>
2019-03-28 10:41:33 -0600

    reverted to typewriter having its own component so the cards dont rerender on each letter typed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-// import TypeWriter from './TypeWriter.js';
+import TypeWriter from './TypeWriter.js';
 // import '../styles/tile.css';
 
 
@@ -13,36 +13,20 @@ class Card extends Component {
         }
     }
     componentDidMount() {
-        this.handleType();
+        // this.handleType();
     }
 
-    handleType = () => {
-        const { animal } = this.props;
-        const { text } = this.state;
-        this.setState({
-          text: animal.substring(0, text.length + 1),
-          typingSpeed: 150
-        });
-    
-  
-      if (text === animal){
-        console.log("DONE");
-        setTimeout(this.props.nextCard(), 5000);
-        
-        return;
-      }
-        setTimeout(this.handleType, 150);
-      };
     render(){
         console.warn("card this.props", this.props);
         
-        const { color } = this.props;
+        const { color, animal } = this.props;
         return (
             <div className="card" style={{background: color}}>
-                <p className="text">
+            <TypeWriter animal={animal}/>
+                {/* <p className="text">
                     <span className="fade-in">{ this.state.text }</span>
                     <span id="cursor"></span>
-                </p>    
+                </p>     */}
             </div> 
         );
     }
